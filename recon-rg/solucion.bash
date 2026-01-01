c5c4d5b9e931a05b8cdaad83e157ced91cd09d8d
Sergio Alejandro <sergioromero2k@gmail.com>
2026-01-02 00:27:41 +0100

    adding challenges

--- recon-rg/solucion.bash
+++ recon-rg/solucion.bash
@@ -1,3 +1,15 @@
+recon -ng
+
+# Reto 1
+workspaces create practica_segura
+db insert domains
+domain(string): tesla.com
+notes(string): ....
+show domains # o puedes poner db query SELECT * FROM domains
 
 
-
+# Reto 2
+marketplace search hosts
+marketplace install recon/domains-hosts/bing_domain_web
+marketplace install recon/domains-hosts/google_site_web
+modules search hosts
